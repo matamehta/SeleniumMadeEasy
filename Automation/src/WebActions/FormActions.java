9766d28e46815da6d55537b7ddd75e1dc2d252ca
mgupta1405 <mguptarocks@yahoo.co.in>
2017-07-16 22:48:41 +0530

    Added FormAction class and its constructor

---
+++ Automation/src/WebActions/FormActions.java
@@ -0,0 +1,13 @@
+package WebActions;
+
+import org.openqa.selenium.WebDriver;
+
+public class FormActions {
+	
+	WebDriver driver;
+	
+	public FormActions(WebDriver driver) {
+		this.driver = driver;
+	}
+
+}
