99e794db8319dc82a598a08f9641931b3db8af4c
mgupta1405 <mguptarocks@yahoo.co.in>
2017-07-22 01:02:10 +0530

    New Feature: Defined ANgular type locators

---
+++ Automation/src/com/seleniummadeeasy/locators/AngularLocators.java
@@ -0,0 +1,13 @@
+package com.seleniummadeeasy.locators;
+
+public class AngularLocators {
+	
+	public enum AngularLocator {
+		ngModel,
+		ngRepeater,
+		ngBind,
+		ngOptions,
+		ngSwitch,
+		ngClass
+	}
+}
