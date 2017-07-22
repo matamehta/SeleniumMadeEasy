abd569986e48510f3f19c15d15e1fdcb8e1bf918
mgupta1405 <mguptarocks@yahoo.co.in>
2017-07-23 01:25:30 +0530

    Added Javadoc and modified logic to getAllAttributes

--- Automation/src/com/seleniummadeeasy/findelement/FindWebElement.java
+++ Automation/src/com/seleniummadeeasy/findelement/FindWebElement.java
@@ -22,6 +22,15 @@ public class FindWebElement {
 		this.driver = driver;
 	}
 	
+	/**
+	 * Finds a web element based on Selenium By locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param byLocator	Selenium By locator of web element
+	 * @return			Selenium WebElement locator
+	 * @throws Exception
+	 */
 	public WebElement findWebElement(By byLocator) throws Exception {
 		WebElement element;
 		try {
@@ -47,6 +56,16 @@ public class FindWebElement {
 		return element;
 	}
 	
+	/**
+	 * Find web element of column in a table
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param rowLocator Selenium WebElement locator of table rows
+	 * @param byColumnLocator Selenium By locator of column in table
+	 * @return				  Selenium WebElement locator
+	 * @throws Exception
+	 */
 	public WebElement findNestedWebElement(WebElement rowLocator, By byColumnLocator) throws Exception {
 		WebElement column;
 		try {
@@ -72,6 +91,15 @@ public class FindWebElement {
 		return column;
 	}
 	
+	/**
+	 * Find all Selenium WebElement using Selenium By locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param byLocator Selenium By locator
+	 * @return			List of Selenium WebElement
+	 * @throws Exception
+	 */
 	public List<WebElement> findWebElements(By byLocator) throws Exception {
 		List<WebElement> elements;
 		try {
@@ -86,6 +114,15 @@ public class FindWebElement {
 		return elements;
 	}
 	
+	/**
+	 * Find Selenium Select locator using Selenium By locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param byLocator Selenium By locator
+	 * @return			Selenium Select locator
+	 * @throws Exception
+	 */
 	public Select findDropdownElement(By byLocator) throws Exception {
 		Select element;
 		try {
@@ -112,18 +149,33 @@ public class FindWebElement {
 		return element;
 	}
 	
+	/**
+	 * Get all attributes in an element
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param elementLocator Selenium WebElement locator
+	 * @return				 List of all attributes in an element
+	 * @throws Exception
+	 */
 	public ArrayList<String> getAllAttributes(WebElement elementLocator) throws Exception {
 		ArrayList<String> attributes;
 		try {
 			JavascriptExecutor js = (JavascriptExecutor) driver;
-			attributes = (ArrayList<String>) js.executeScript(
+			Object objectAttribute = js.executeScript(
 					"var items = {};"
 				  + "for (index = 0; index < arguments[0].attributes.length; ++index)"
 				  + "{ "
 				  + "items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value"
 				  + " };"
-				  + " return items;",
+				  + "var list = [];"
+				  + "Object.keys(items).forEach(function(key) {"
+				  + "		list.push(key)"
+				  + "});"
+				  + " return list;",
 				  elementLocator);
+			
+			attributes = (ArrayList<String>) objectAttribute;
 		}
 		catch(Exception e) {
 			throw new Exception(e.getCause().toString());
@@ -131,6 +183,13 @@ public class FindWebElement {
 		return attributes;
 	}
 	
+	/**
+	 * Scroll to element  specified by Selenium WebElement
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param elementLocator Selenium WebElement locator
+	 */
 	public void scrollToWebElement(WebElement elementLocator) {
 		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true)", elementLocator);
 	}
