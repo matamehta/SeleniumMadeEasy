c9bf07d39536ff639930fa62c6dfcd08ba279107
mgupta1405 <mguptarocks@yahoo.co.in>
2017-07-23 22:32:20 +0530

    Added Javadoc: FormActionsOnAngularApp.java

--- Automation/src/com/seleniummadeeasy/webactions/FormActionsOnAngularApp.java
+++ Automation/src/com/seleniummadeeasy/webactions/FormActionsOnAngularApp.java
@@ -25,6 +25,16 @@ public class FormActionsOnAngularApp {
 		findAngularElement = new FindAngularElement(this.driver);
 	}
 	
+	/**
+	 * Enter text in textbox using Angular specific locators
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @param text Text to enter in textbox
+	 * @throws Exception
+	 */
 	public void enterText(AngularLocator locator, String locatorValue,  String text) throws Exception {
 		try {
 			WebElement element = findAngularElement.getWebElementFromAngularLocator(locator, locatorValue);
@@ -35,6 +45,15 @@ public class FormActionsOnAngularApp {
 		}
 	}
 	
+	/**
+	 * Click on web element using Angular specific locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @throws Exception
+	 */
 	public void click(AngularLocator locator, String locatorValue) throws Exception {
 		try {
 			WebElement element = findAngularElement.getWebElementFromAngularLocator(locator, locatorValue);
@@ -45,6 +64,15 @@ public class FormActionsOnAngularApp {
 		}
 	}
 	
+	/**
+	 * Check on web element using Angular specific locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @throws Exception
+	 */
 	public void check(AngularLocator locator, String locatorValue) throws Exception {
 		try {
 			WebElement element = findAngularElement.getWebElementFromAngularLocator(locator, locatorValue);
@@ -55,6 +83,15 @@ public class FormActionsOnAngularApp {
 		}
 	}
 	
+	/**
+	 * UnCheck on web element using Angular specific locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @throws Exception
+	 */
 	public void uncheck(AngularLocator locator, String locatorValue) throws Exception {
 		try {
 			WebElement element = findAngularElement.getWebElementFromAngularLocator(locator, locatorValue);
@@ -65,6 +102,15 @@ public class FormActionsOnAngularApp {
 		}
 	}
 	
+	/**
+	 * Select radio button using Angular specific locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @throws Exception
+	 */
 	public void selectRadio(AngularLocator locator, String locatorValue) throws Exception {
 		try {
 			WebElement element = findAngularElement.getWebElementFromAngularLocator(locator, locatorValue);
@@ -75,6 +121,16 @@ public class FormActionsOnAngularApp {
 		}
 	}
 	
+	/**
+	 * Select dropdown via Option text using Angular specific locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	* @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @param option Text of option to select
+	 * @throws Exception
+	 */
 	public void selectDropdownByText(AngularLocator locator, String locatorValue, String option) throws Exception {
 		try {
 			WebElement element = findAngularElement.getWebElementFromAngularLocator(locator, locatorValue);
@@ -85,16 +141,36 @@ public class FormActionsOnAngularApp {
 		}
 	}
 	
-	public void selectDropdownByValue(AngularLocator locator, String locatorValue, String option) throws Exception {
+	/**
+	 * Select dropdown via Value using Angular specific locator 
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	* @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @param value Text of value to select
+	 * @throws Exception
+	 */
+	public void selectDropdownByValue(AngularLocator locator, String locatorValue, String value) throws Exception {
 		try {
 			WebElement element = findAngularElement.getWebElementFromAngularLocator(locator, locatorValue);
-			formActions.selectDropdownByValueUsingWebElement((Select)element, option);
+			formActions.selectDropdownByValueUsingWebElement((Select)element, value);
 		}
 		catch(Exception e) {
 			throw new Exception(e.getCause().toString());
 		}
 	}
 	
+	/**
+	 * Select dropdown via Index using Angular specific locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @param index Index of value to select
+	 * @throws Exception
+	 */
 	public void selectDropdownByIndex(AngularLocator locator, String locatorValue, int index) throws Exception {
 		try {
 			WebElement element = findAngularElement.getWebElementFromAngularLocator(locator, locatorValue);
@@ -105,6 +181,15 @@ public class FormActionsOnAngularApp {
 		}
 	}
 	
+	/**
+	 * Hover on web element using Angular specific locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @throws Exception
+	 */
 	public void hover(AngularLocator locator, String locatorValue) throws Exception {
 		try {
 			WebElement element = findAngularElement.getWebElementFromAngularLocator(locator, locatorValue);
@@ -115,6 +200,16 @@ public class FormActionsOnAngularApp {
 		}
 	}
 	
+	/**
+	 * Get text of web element using Angular specific locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @return			   Text to return from web element
+	 * @throws Exception
+	 */
 	public String getText(AngularLocator locator, String locatorValue) throws Exception {
 		String textToReturn = null;
 		try {
@@ -127,6 +222,16 @@ public class FormActionsOnAngularApp {
 		return textToReturn;
 	}
 	
+	/**
+	 * Get text of web element using Angular specific locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @return			   Text to return from web element
+	 * @throws Exception
+	 */
 	public String getTextFromTextbox(AngularLocator locator, String locatorValue) throws Exception {
 		String textToReturn = null;
 		try {
@@ -139,6 +244,17 @@ public class FormActionsOnAngularApp {
 		return textToReturn;
 	}
 	
+	/**
+	 * Get attribute value using Angular specific locator
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	 * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @param attributeName Attribute name whose value is to be retrieved
+	 * @return				Value of given attribute
+	 * @throws Exception
+	 */
 	public String getAttribute(AngularLocator locator, String locatorValue, String attributeName) throws Exception {
 		String textToReturn = null;
 		try {
@@ -151,6 +267,16 @@ public class FormActionsOnAngularApp {
 		return textToReturn;
 	}
 	
+	/**
+	 * Is web element with Angular specific locator visible on screen
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	  * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @return			   true if element is visible, false otherwise
+	 * @throws Exception
+	 */
 	public Boolean isElementVisible(AngularLocator locator, String locatorValue) throws Exception {
 		Boolean isElementVisible = null;
 		try {
@@ -173,6 +299,16 @@ public class FormActionsOnAngularApp {
 		return isElementVisible;
 	}
 	
+	/**
+	 * Is web element with Angular specific locator enable on screen
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	  * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @return			   true if element is enable, false otherwise
+	 * @throws Exception
+	 */
 	public Boolean isElementEnable(AngularLocator locator, String locatorValue) throws Exception {
 		Boolean isElementEnable = null;
 		try {
@@ -185,6 +321,16 @@ public class FormActionsOnAngularApp {
 		return isElementEnable;
 	}
 	
+	/**
+	 * Is web element with Angular specific locator present in HTML on screen
+	 * 
+	 * @author Mohit Gupta
+	 * 
+	  * @param locator Angular locator
+	 * @param locatorValue Value of angular attribute
+	 * @return			   true if element is present in HTML, false otherwise
+	 * @throws Exception
+	 */
 	public Boolean isElementPresent(AngularLocator locator, String locatorValue) throws Exception {
 		Boolean isElementPresent = null;
 		List<WebElement> elements;
